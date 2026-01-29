0390933d3dd123c270216af1edf40e43a8fc9c55
Sabharish <sabharishhhpv2304@gmail.com>
2026-01-29 12:48:21 +0530

    get product by referring id

--- src/main/java/org/example/ecomapp/controller/ProductController.java
+++ src/main/java/org/example/ecomapp/controller/ProductController.java
@@ -3,6 +3,9 @@ package org.example.ecomapp.controller;
 import org.example.ecomapp.model.Product;
 import org.example.ecomapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +24,13 @@ public class ProductController {
     }
 
     @RequestMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @RequestMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        System.out.println("prod: " + id + "data requested");
+        return new ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
     }
 }
--- src/main/java/org/example/ecomapp/model/Product.java
+++ src/main/java/org/example/ecomapp/model/Product.java
@@ -1,5 +1,6 @@
 package org.example.ecomapp.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,7 +26,10 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
+
     private int quantity;
     private boolean available;
 
